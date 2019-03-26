b1250f712303adb58d2be5d26446b78d16d8a096
Maciej Kluczyński <maciej.lukasz.kluczynski@gmail.com>
2019-03-26 21:06:30 +0100

    add overlaying and saving images

--- __init__.py
+++ __init__.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
 
+import os
 import argparse
+import datetime
 import matplotlib.pyplot as plt
 import pandas as pd
+import random
 
 from pyexcel_ods3 import get_data
+from PIL import Image
 
 
 def main():
@@ -12,28 +16,37 @@ def main():
 
     parser.add_argument('file_path', metavar='path to file', type=str, nargs=1,
                         help='path to the ods file')
-    parser.add_argument('-w', metavar='wallpaper folder', type=str, nargs=1,
-                        help='directory where wallpapers are stored')
+    parser.add_argument('-sw', metavar='source wallpaper directory', type=str, nargs=1,
+                        help='directory where source wallpapers are stored')
+    parser.add_argument('-ow', metavar='output wallpaper directory', type=str, nargs=1,
+                        help='directory where output wallpapers will be saved')
+    parser.add_argument('-af', metavar='appearance frequency in percent', type=float,
+                        nargs=1, help='percentage of wallpapers that will be overlaid')
     args = parser.parse_args()
 
     ods_data = get_data(args.file_path.pop())
-    wallpaper_folder = args.w.pop()
+    src_wallpaper_dir = args.sw.pop()
+    out_wallpaper_dir = args.ow.pop()
+    appearance_frequency = args.af.pop()
 
-    WeightGazer(ods_data, wallpaper_folder)
+    WeightGazer(ods_data,
+                src_wallpaper_dir,
+                out_wallpaper_dir,
+                appearance_frequency,
+                )
 
 
 class WeightGazer:
 
-    def __init__(self, ods_data, wallpaper_folder):
+    def __init__(self, ods_data, src_wallpaper_dir, out_wallpaper_dir, appearance_frequency):
         self.ods_data = ods_data
-        self.wallpaper_folder = wallpaper_folder
         self.columns = None
         self.meals = None
         self.df = None
         self.prepare_dataframe()
         self.fig = self.draw_figure()
+        FileWriter(self.fig, src_wallpaper_dir, out_wallpaper_dir, appearance_frequency)
 
-        plt.savefig('/home/mklucz/Desktop/weight.png', transparent=True)
 
     def prepare_dataframe(self):
         df = pd.DataFrame(self.ods_data['Sheet1'][1:])
@@ -63,11 +76,53 @@ class WeightGazer:
         ax2.plot(
             ax.get_xticks(),
             self.df['weight'],
+            ls='--',
             lw=10,
             color='pink',
+            marker='o',
+            markersize=20,
         )
         return plt.gcf()
 
+class FileWriter:
+
+    def __init__(self, fig, src_wallpaper_dir, out_wallpaper_dir, appearance_frequency):
+        self.fig = fig
+        self.src_wallpaper_dir = src_wallpaper_dir
+        self.out_wallpaper_dir = out_wallpaper_dir
+        src_files = os.listdir(self.src_wallpaper_dir)
+        self.temp_file = self.save_temp_file()
+        self.save_files(src_files, appearance_frequency)
+
+    def save_temp_file(self):
+        timestamp = str(datetime.datetime.utcnow()).replace(" ", "_")
+        temp_file_name = 'weight-gazer-temp-{}.png'.format(timestamp)
+        temp_file_path = os.path.join(self.src_wallpaper_dir, temp_file_name)
+        plt.savefig(temp_file_path, transparent=True)
+        return temp_file_path
+
+    def save_files(self, src_files, appearance_frequency):
+        images_to_overlay = random.sample(
+            src_files,
+            int(appearance_frequency/100*len(src_files)),
+        )
+        for background in images_to_overlay:
+            timestamp = str(datetime.datetime.utcnow()).replace(" ", "_")
+            filename = '{}-weight-gazer-{}.png'.format(background, timestamp)
+            try:
+                self.overlay_image(
+                    os.path.join(self.src_wallpaper_dir, background),
+                    self.temp_file,
+                    filename,
+                )
+            except Exception:
+                continue
+
+    def overlay_image(self, background, foreground, filename):
+        wallpaper = Image.open(background)
+        chart = Image.open(foreground)
+        wallpaper.paste(chart, (0, 0), chart)
+        wallpaper.save(os.path.join(self.out_wallpaper_dir, filename))
 
 if __name__ == "__main__":
     main()
